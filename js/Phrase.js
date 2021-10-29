9fd299537536a58764aabd55c655eb07d226f8c3
GerazimCode <gerazimcode@gmail.com>
2021-10-29 12:54:12 -0400

    add the addPhraseToDisplay method to the phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -12,6 +12,16 @@ class Phrase {
 
     // Methods adds letter placeholders to the display when the game starts
     addPhraseToDisplay(){
-
+        let splitPhrase = this.phrase.split('');
+        let phraseDiv = document.getElementById("phrase");
+        splitPhrase.forEach(character => {
+            let spaceCharacter = `<li class = "space"> </li>`;
+            let letterCharacter = `<li class = "hide letter ${character}">${character}</li>`;
+            if (character === " "){
+                phraseDiv.querySelector("ul").insertAdjacentHTML("beforeend", spaceCharacter);
+            }else{
+                phraseDiv.querySelector("ul").insertAdjacentHTML("beforeend", letterCharacter);
+            }
+        })
     }
 }
--- js/app.js
+++ js/app.js
@@ -4,11 +4,6 @@
 
 // This class create new instances of the game class and event listeners for the start button and on screen keyboard buttons.
 
-let logPhrase = (phrase) => {
-    console.log(`phrase - phrase:`, phrase.phrase);
-}
-
 let game = new Game();
 
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
+game.getRandomPhrase().addPhraseToDisplay();
